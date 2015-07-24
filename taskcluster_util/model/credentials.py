3a4e7c64a821d57902cad78ba7d1e3c648600232
Johan Lorenzo <jlorenzo@mozilla.com>
2015-07-24 17:06:54 +0200

    Let the user use a JSON object as a certificate

--- taskcluster_util/model/credentials.py
+++ taskcluster_util/model/credentials.py
@@ -13,8 +13,20 @@ class Credentials(dict):
             # TODO: Replace by log.warning once the loggers works.
             print '''Warning: no certificate is present in the given credentials. If the credentials are temporary you won't be able to download anything'''
 
+    def __getitem__(self, item):
+        value = dict.__getitem__(self, item)
+        # The taskcluster client always expects each argument of the Credentials to be a string,
+        # even for the certificate. To let the user copy and paste the certificate directly from
+        # https://auth.taskcluster.net/ without escaping all the quotes, we perform this change
+        # of type.
+        return self._enforce_string(value)
+
     @staticmethod
     def from_file(file_path):
         with open(file_path) as fd:
             credentials = json.load(fd)
         return Credentials(**credentials)
+
+    @staticmethod
+    def _enforce_string(value):
+        return value if type(value) in (str, unicode) else json.dumps(value, ensure_ascii=True)
