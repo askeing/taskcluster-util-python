0420a9f41a043dae5ff28bd95cadb384b552d2a3
askeing <askeing@gmail.com>
2015-07-28 17:43:26 +0800

    Fix the logging issues - fix #1

--- taskcluster_util/taskcluster_download.py
+++ taskcluster_util/taskcluster_download.py
@@ -16,13 +16,11 @@ from taskcluster.exceptions import TaskclusterAuthFailure, TaskclusterRestFailur
 from model.credentials import Credentials
 
 
-log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
-
-
 class DownloadRunner(object):
 
     def __init__(self):
+        logger = logging.getLogger(__name__)
+
         # argument parser
         taskcluster_credentials = 'tc_credentials.json'
         parser = argparse.ArgumentParser(prog='taskcluster_download', description='The simple download tool for Taskcluster.',
@@ -41,9 +39,17 @@ class DownloadRunner(object):
         artifact_group = parser.add_argument_group('Download Artifact', 'The artifact name and dest folder')
         artifact_group.add_argument('-a', '--artifact', action='store', dest='aritfact_name', help='The artifact name on Taskcluster')
         artifact_group.add_argument('-d', '--dest-dir', action='store', dest='dest_dir', help='The dest folder')
+        parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', default=False, help='Turn on verbose output, with all the debug logger.')
 
         # parser the argv
         self.options = parser.parse_args(sys.argv[1:])
+        # setup the logging config
+        if self.options.verbose is True:
+            verbose_formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+            logging.basicConfig(level=logging.DEBUG, format=verbose_formatter)
+        else:
+            formatter = '%(levelname)s: %(message)s'
+            logging.basicConfig(level=logging.INFO, format=formatter)
 
     def show_latest_artifacts(self, artifact_downloader, task_id):
         print('### Getting latest artifacts of TaskID {} ...'.format(task_id))
@@ -81,7 +87,7 @@ class DownloadRunner(object):
                 print('### The TaskID of Namespace [{}] is [{}].'.format(task_namespace, task_id))
             except TaskclusterRestFailure as e:
                 print('### Can not get the TaskID due to [{}]'.format(e.message))
-                log.error(e.body)
+                logger.error(e.body)
                 exit(-1)
         else:
             task_id = self.options.task_id
@@ -105,7 +111,7 @@ class DownloadRunner(object):
                 local_file = artifact_downloader.download_latest_artifact(task_id, self.options.aritfact_name, abs_dest_dir)
             except (TaskclusterAuthFailure, TaskclusterRestFailure) as e:
                 print('### Can not download due to [{}]'.format(e.message))
-                log.error(e.body)
+                logger.error(e.body)
                 exit(-1)
             print('### Download [{}] from TaskID [{}] to [{}] done.'.format(self.options.aritfact_name, task_id, local_file))
 
