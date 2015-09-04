987c51695b05d268f1497266551581ee78366394
askeing <askeing@gmail.com>
2015-09-04 11:35:01 +0800

    Update to 0.0.11

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,6 +1,21 @@
 Release History
 ---------------
 
+0.0.11 (2015-09-04)
+++++++++++++++++++
+
+**Features and Improvements**
+
+- Add taskcluster_traverse
+- Modify setup.py, HISTORY, and README.
+- Add more function of TaskFinder.
+- Add Makefile and travis ci settings.
+- Add unittest cases.
+
+**Bugfixes**
+
+- Fix the temp folder deleted issue when downloading multiple times
+
 0.0.10 (2015-08-04)
 ++++++++++++++++++
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 
-VERSION='0.0.10'
+VERSION = '0.0.11'
 
 # TODO:
 # Bug 1184209 - reference-format: Handle end-points that returns a stream (Discussion)
@@ -30,23 +30,23 @@ long_description = readme + '\n\n' + history
 
 
 if __name__ == '__main__':
-  setup(
-    name='taskcluster_util',
-    version=VERSION,
-    description='Taskcluster Utilities',
-    long_description=long_description,
-    keywords='taskcluster utilities download ',
-    author='Askeing Yen',
-    author_email='askeing@gmail.com',
-    url='https://github.com/askeing/taskcluster-util-python',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    package_data={},
-    install_requires=install_requires,
-    zip_safe=False,
-    entry_points="""
-        # -*- Entry points: -*-
-        [console_scripts]
-        taskcluster_download = taskcluster_util.taskcluster_download:main
-        taskcluster_traverse = taskcluster_util.taskcluster_traverse:main
-        """,
+    setup(
+          name='taskcluster_util',
+          version=VERSION,
+          description='Taskcluster Utilities',
+          long_description=long_description,
+          keywords='taskcluster utilities download ',
+          author='Askeing Yen',
+          author_email='askeing@gmail.com',
+          url='https://github.com/askeing/taskcluster-util-python',
+          packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+          package_data={},
+          install_requires=install_requires,
+          zip_safe=False,
+          entry_points="""
+          # -*- Entry points: -*-
+          [console_scripts]
+          taskcluster_download = taskcluster_util.taskcluster_download:main
+          taskcluster_traverse = taskcluster_util.taskcluster_traverse:main
+          """,
   )
