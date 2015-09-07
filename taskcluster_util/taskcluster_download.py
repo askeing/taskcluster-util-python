743c73edf0321194c31c5f211f3e79317d501d17
askeing <askeing@gmail.com>
2015-09-07 16:57:32 +0800

    Refactoring taskcluster_download

--- taskcluster_util/taskcluster_download.py
+++ taskcluster_util/taskcluster_download.py
@@ -4,7 +4,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import os
-import shutil
 import logging
 import argparse
 import textwrap
@@ -12,7 +11,6 @@ from argparse import RawTextHelpFormatter
 
 from util.finder import *
 from util.downloader import *
-from taskcluster.exceptions import TaskclusterAuthFailure, TaskclusterRestFailure
 from model.credentials import Credentials
 
 
@@ -20,12 +18,23 @@ logger = logging.getLogger(__name__)
 
 
 class DownloadRunner(object):
+    def __init__(self, connection_options={}):
+        """
+        @param connection_options: the options argument for connection. e.g. {'credentials': ...}
+        """
+        self.connection_options = connection_options
+        self.namespace = None
+        self.task_id = None
+        self.aritfact_name = None
+        self.dest_dir = None
 
-    def __init__(self):
+    def cli(self):
+        """
+        This method will parse the argument for CLI.
+        """
         # argument parser
         taskcluster_credentials = 'tc_credentials.json'
-        parser = argparse.ArgumentParser(prog='taskcluster_download',
-                                         description='The simple download tool for Taskcluster.',
+        parser = argparse.ArgumentParser(prog='taskcluster_download', description='The simple download tool for Taskcluster.',
                                          formatter_class=RawTextHelpFormatter,
                                          epilog=textwrap.dedent('''\
                                          The tc_credentials.json Template:
@@ -44,16 +53,31 @@ class DownloadRunner(object):
         parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', default=False, help='Turn on verbose output, with all the debug logger.')
 
         # parser the argv
-        self.options = parser.parse_args(sys.argv[1:])
+        options = parser.parse_args(sys.argv[1:])
         # setup the logging config
-        if self.options.verbose is True:
+        if options.verbose is True:
             verbose_formatter = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
             logging.basicConfig(level=logging.DEBUG, format=verbose_formatter)
         else:
             formatter = '%(levelname)s: %(message)s'
             logging.basicConfig(level=logging.INFO, format=formatter)
+        # check credentials file
+        abs_credentials_path = os.path.abspath(options.credentials)
+        credentials = Credentials.from_file(abs_credentials_path)
+        self.connection_options = {'credentials': credentials}
+        # assign the variable
+        self.namespace = options.namespace
+        self.task_id = options.task_id
+        self.aritfact_name = options.aritfact_name
+        self.dest_dir = options.dest_dir
+        return self
 
     def show_latest_artifacts(self, artifact_downloader, task_id):
+        """
+        Print the artifacts by given TaskId.
+        @param artifact_downloader: Downloader object with connection options.
+        @param task_id: the given TaskId.
+        """
         logger.info('Getting latest artifacts of TaskID {} ...'.format(task_id))
         ret = artifact_downloader.get_latest_artifacts(task_id)
         artifacts_list = ret.get('artifacts')
@@ -63,43 +87,40 @@ class DownloadRunner(object):
             print('{}| {}'.format(artifact.get('contentType').ljust(width), artifact.get('name').ljust(width)))
 
     def run(self):
-        # check credentials file
-        abs_credentials_path = os.path.abspath(self.options.credentials)
-
-        credentials = Credentials.from_file(abs_credentials_path)
-        connection_options = {'credentials': credentials}
-
-        if self.options.namespace is not None:
+        """
+        Run the download process.
+        """
+        if self.namespace is not None:
             # remove the 'index.' and 'root.' of namespace
-            task_namespace = self.options.namespace
+            task_namespace = self.namespace
             logger.info('Finding the TaskID of Namespace [{}] ...'.format(task_namespace))
-            if self.options.namespace.startswith('index.'):
-                task_namespace = self.options.namespace[len('index.'):]
+            if self.namespace.startswith('index.'):
+                task_namespace = self.namespace[len('index.'):]
                 logger.info('Remove the ["index."] of Namespace [{}].'.format(task_namespace))
-            elif self.options.namespace.startswith('root.'):
-                task_namespace = self.options.namespace[len('root.'):]
+            elif self.namespace.startswith('root.'):
+                task_namespace = self.namespace[len('root.'):]
                 logger.info('Remove the ["root."] of Namespace [{}].'.format(task_namespace))
             # find TaskId from Namespace
-            task_finder = TaskFinder(connection_options)
+            task_finder = TaskFinder(self.connection_options)
             task_id = task_finder.get_taskid_by_namespace(task_namespace)
             logger.info('The TaskID of Namespace [{}] is [{}].'.format(task_namespace, task_id))
         else:
-            task_id = self.options.task_id
+            task_id = self.task_id
 
-        artifact_downloader = Downloader(connection_options)
-        if self.options.aritfact_name is None:
+        artifact_downloader = Downloader(self.connection_options)
+        if self.aritfact_name is None:
             # no artifact_name, then get the latest artifacts list
             self.show_latest_artifacts(artifact_downloader, task_id)
         else:
             # has artifact_name, then download it
-            logger.info('Downloading latest artifact [{}] of TaskID [{}] ...'.format(self.options.aritfact_name, task_id))
-            local_file = artifact_downloader.download_latest_artifact(task_id, self.options.aritfact_name, self.options.dest_dir)
-            logger.info('Download [{}] from TaskID [{}] to [{}] done.'.format(self.options.aritfact_name, task_id, local_file))
+            logger.info('Downloading latest artifact [{}] of TaskID [{}] ...'.format(self.aritfact_name, task_id))
+            local_file = artifact_downloader.download_latest_artifact(task_id, self.aritfact_name, self.dest_dir)
+            logger.info('Download [{}] from TaskID [{}] to [{}] done.'.format(self.aritfact_name, task_id, local_file))
 
 
 def main():
     try:
-        DownloadRunner().run()
+        DownloadRunner().cli().run()
     except Exception as e:
         logger.error(e)
         if e.__dict__:
